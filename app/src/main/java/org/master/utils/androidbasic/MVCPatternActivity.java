6b6554119a42f00777efea9bb425f127a3e73d57
gurucm <gc6719@gmail.com>
2018-04-02 18:40:25 +0530

    Initial commit

---
+++ app/src/main/java/org/master/utils/androidbasic/MVCPatternActivity.java
@@ -0,0 +1,13 @@
+package org.master.utils.androidbasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MVCPatternActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mvcpattern);
+    }
+}
